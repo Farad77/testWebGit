f7da4add90d94d1b8812aa7733c51b655da91223
ECR_ <etilol97421@gmail.com>
2023-10-17 12:55:27 +0400

    modification du fichier etienne.php

--- Etienne/etienne.php
+++ Etienne/etienne.php
@@ -1,19 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8">
+        <title>testWebGit</title>
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <title>testWebGit</title>
-</head>
+    <body>
+        <?php
 
-<body>
-    <?php
+        $age = 20;
+        $amis = array("Pierre", "Paul", "Jaques");
 
-    $age = 20;
+        var_dump($amis);
 
-    echo "Etienne à " . $age . " ans.";
+        echo "Etienne à " . $age . " ans.";
+        echo "<br/>";
 
-    ?>
-</body>
+        echo "Son 1er amis est " . $amis[0];
+        echo "<br/>";
+        echo "Son 2eme amis est " . $amis[1];
+        echo "<br/>";
+        echo "Son 3eme amis est " . $amis[2];
+        echo "<br/>";
+        echo "Son 4eme amis est " . $amis[3];
+        echo "<br/>";
 
+        ?>
+    </body>
 </html>
